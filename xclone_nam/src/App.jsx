71cffed9208a6962d7f44568f3a713795e6b565d
AnaMorales4 <alt.anamor4@gmail.com>
2024-10-09 14:18:22 -0500

    Maquetado de la página LogUp

--- xclone_nam/src/App.jsx
+++ xclone_nam/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Login } from './login/Login'
+import { Logup } from './logup/Logup'
+
 
 function App() {
 
   return (
     <>
       <Login />
+      <Logup/> 
     </>
   )
 }
